e8826470d18601f09ef1a8ca836c27a06902d4c2
Flavio Gazzetta <flavio.gazzetta.2005@gmail.com>
2024-11-27 22:27:22 +0000

    added second LeetCode exercise: reverse_integer - medium

---
+++ Medium/reverse_integer.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+
+class Solution {
+public:
+    int reverse(int x) {
+
+        std::string str = std::to_string(x);
+
+        bool neg = false;
+
+        char temp;
+
+        if(str[0] == '-'){
+
+            str = str.substr(1);
+
+            neg = true;
+
+        }
+
+        for(int i = 0; i < str.length()/2; i++){
+
+            temp = str[i];
+
+            str[i] = str[str.length() - 1 - i];
+
+            str[str.length() - 1 - i] = temp;
+
+        }
+        
+        long reversedNum = std::stol(str);
+
+        if(neg) {
+
+            reversedNum = -reversedNum;
+
+        }
+            
+        if(reversedNum < std::numeric_limits<int>::min() || reversedNum > std::numeric_limits<int>::max()) {
+
+            return 0;
+
+        }
+
+        int out = reversedNum;
+
+        return out;
+
+    }
+};
