05386c5c8f9a31d70b7b4fc25e1d65088542f750
Flavio Gazzetta <flavio.gazzetta.2005@gmail.com>
2024-12-03 10:30:40 +0000

    added medium difficulty: 3 - Longest substring without repeating charachters

---
+++ 2)Medium/3.Longest_substring_without_repeating_charachters.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+
+        std::unordered_set<char> charSet; 
+
+        int left = 0, maxLength = 0;
+
+        for (int right = 0; right < s.size(); ++right) {
+            
+            while (charSet.find(s[right]) != charSet.end()) {
+
+                charSet.erase(s[left]);
+
+                left++;
+
+            }
+            
+            charSet.insert(s[right]);
+           
+            maxLength = std::max(maxLength, right - left + 1);
+
+        }
+
+        return maxLength;
+        
+    }
+};
